0b1ddfacadef001dda27141129a60dd959dd8653
dylanfeehan <dylanfeehan981@gmail.com>
2023-02-27 13:49:56 -0500

    ch

--- something.py
+++
@@ -1,7 +0,0 @@
-name = input("Enter your name>>>");
-age = input("Enter your age>>>");
-print("Hello " + name + "!");
-weeks = 4000
-weeksUsed = int(age) * 50
-weeksLeft = weeks - weeksUsed
-print("You have " + str(weeksLeft) + " weeks left to live, on average.")
