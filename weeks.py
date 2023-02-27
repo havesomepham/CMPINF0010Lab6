0e565a22eb6e28156f58e905873f7a98f1225475
dylanfeehan <dylanfeehan981@gmail.com>
2023-02-27 13:48:58 -0500

    changed name

---
+++ weeks.py
@@ -0,0 +1,7 @@
+name = input("Enter your name>>>");
+age = input("Enter your age>>>");
+print("Hello " + name + "!");
+weeks = 4000
+weeksUsed = int(age) * 50
+weeksLeft = weeks - weeksUsed
+print("You have " + str(weeksLeft) + " weeks left to live, on average.")
